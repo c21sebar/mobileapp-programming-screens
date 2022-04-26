f4c04a9f7fc8f623492bdbcd05f401bc5281be66
Sebastian Arneson <c21sebar@student.his.se>
2022-04-26 09:14:17 +0200

    Added code for button to start second activity with intent Added second activity in AndroidManifest.xml

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainButton = findViewById(R.id.MainButton);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            clickToSecondActivity();
+            }
+        });
+    }
+    void clickToSecondActivity(){
+        Intent myIntent = new Intent(this, SecondActivity.class);
+        myIntent.putExtra("name","value");
+        startActivity(myIntent);
+
     }
 }
