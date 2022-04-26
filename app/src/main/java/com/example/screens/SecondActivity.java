24a44feb2c221b305a3f0eb6b353ef68dd0902df
Sebastian Arneson <c21sebar@student.his.se>
2022-04-26 09:15:20 +0200

    Added code in Second activity to recive inent and display the value in the textView

---
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.screens;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    Button mySecondButton;
+    TextView secondTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        mySecondButton = findViewById(R.id.SecondActivityButton);
+        secondTextView = findViewById(R.id.SecondActivityTextView);
+        final String name = getIntent().getExtras().getString("name");
+        secondTextView.setText(name);
+
+
+    }
+}
